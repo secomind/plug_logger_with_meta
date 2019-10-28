b10d0ad096b7ec50a341391e01d68db04b5ba592
Davide Bettio <davide.bettio@ispirata.com>
2019-10-28 15:26:29 +0100

    mix new plug_metadata_logger

---
+++ lib/plug_metadata_logger.ex
@@ -0,0 +1,18 @@
+defmodule PlugMetadataLogger do
+  @moduledoc """
+  Documentation for PlugMetadataLogger.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PlugMetadataLogger.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/plug_metadata_logger_test.exs
@@ -0,0 +1,8 @@
+defmodule PlugMetadataLoggerTest do
+  use ExUnit.Case
+  doctest PlugMetadataLogger
+
+  test "greets the world" do
+    assert PlugMetadataLogger.hello() == :world
+  end
+end
